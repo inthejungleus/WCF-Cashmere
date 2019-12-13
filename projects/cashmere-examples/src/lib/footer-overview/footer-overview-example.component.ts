f747fb04f92c65e6e09e2fa8b0d00aa926b6a8c6
Travis Foxley <tfoxley@wcf.com>
2019-12-13 14:32:38 -0700

    feat(footer): add a footer component

---
+++ projects/cashmere-examples/src/lib/footer-overview/footer-overview-example.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'hc-footer-overview-example',
+    templateUrl: 'footer-overview-example.component.html',
+    styleUrls: ['footer-overview-example.component.scss']
+})
+export class FooterOverviewExampleComponent {
+}
