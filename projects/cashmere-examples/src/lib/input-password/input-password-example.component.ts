462361dcc076dbabd3fcf1a1917dcfaa636ce831
jlewis <jlewis@wcf.com>
2020-03-09 14:40:57 -0600

    feat(password example): password example with show hide eye button

---
+++ projects/cashmere-examples/src/lib/input-password/input-password-example.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hc-input-password-example',
+  templateUrl: './input-password-example.component.html',
+  styleUrls: ['./input-password-example.component.scss']
+})
+export class InputPasswordExampleComponent implements OnInit {
+
+  hiddenEye = false;
+  private pass: HTMLElement;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  togglePassword(){
+    this.hiddenEye = !this.hiddenEye;
+    this.pass = document.getElementById("password") as HTMLElement;
+    if(this.hiddenEye){
+      this.pass.setAttribute("type", "text");
+    } else {
+      this.pass.setAttribute("type", "password");
+    }
+
+  }
+
+}
