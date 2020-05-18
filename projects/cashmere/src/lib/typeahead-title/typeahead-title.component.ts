265d7f8fc50d859099a6a8a3a8f7aea4203291e9
Travis Foxley <tfoxley@wcf.com>
2020-05-18 16:09:15 -0600

    fix(typeahead/title): refine the typeahead/title components

--- projects/cashmere/src/lib/typeahead-title/typeahead-title.component.ts
+++ projects/cashmere/src/lib/typeahead-title/typeahead-title.component.ts
@@ -1,33 +1,25 @@
-import {
-    AfterViewInit,
-    Component,
-    ContentChildren,
-    Input,
-    QueryList,
-} from '@angular/core';
+import {AfterViewInit, Component, ContentChildren, QueryList,} from '@angular/core';
 
-import { TypeaheadComponent } from '../typeahead/typeahead.component';
+import {TypeaheadComponent} from '../typeahead/typeahead.component';
 
 @Component({
-  selector: 'hc-typeahead-title',
-  templateUrl: './typeahead-title.component.html',
-  styleUrls: ['./typeahead-title.component.scss']
+    selector: 'hc-typeahead-title',
+    templateUrl: './typeahead-title.component.html',
+    styleUrls: ['./typeahead-title.component.scss']
 })
 export class TypeaheadTitleComponent implements AfterViewInit {
+    DEFAULT_PLACEHOLDER = 'Typeahead Placeholder';
 
-    isTypeaheadShown: boolean = false;
-    updated: boolean = false;
-
-    @Input()
-    startingValue: any;
+    _isTypeaheadShown: boolean = false;
+    _value: string;
+    _placeholder: string;
+    _displayValue: string;
 
     @ContentChildren(TypeaheadComponent)
     _typeahead: QueryList<TypeaheadComponent>;
 
-    constructor() { }
-
-    hideTitle() {
-        this.isTypeaheadShown = true;
+    _hideTitle() {
+        this._isTypeaheadShown = true;
         setTimeout(() => {
             this._typeahead.first.setFocus();
         });
@@ -35,24 +27,32 @@ export class TypeaheadTitleComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         if (this._typeahead.first) {
+            this._placeholder = this._typeahead.first._inputRef.nativeElement.getAttribute('placeholder');
             this._typeahead.first.optionSelected.subscribe(option => {
-                this.isTypeaheadShown = false;
+                this._isTypeaheadShown = false;
             });
 
-            this._typeahead.first.registerOnChange(this.updateStartingValue.bind(this));
+            this._typeahead.first.registerOnChange(this._updateValue.bind(this));
 
             this._typeahead.first.blur.subscribe(event => {
                 // Prevents the click on the result panel causing the typeahead to disappear before the value can be sent
                 if (this._typeahead.first._resultPanelHidden === true) {
-                    this.isTypeaheadShown = false;
+                    this._isTypeaheadShown = false;
                 }
             });
         }
+
+        this._refreshDisplayValue();
     }
 
-    updateStartingValue(val: string) {
-        this.startingValue = val;
-        this.updated = true;
+    private _updateValue(val: string) {
+        this._value = val;
+        this._refreshDisplayValue();
     }
 
+    private _refreshDisplayValue() {
+        setTimeout(() => {
+            this._displayValue = this._value || this._placeholder || this.DEFAULT_PLACEHOLDER;
+        });
+    }
 }
